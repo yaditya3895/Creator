4774c6ce6e5cb4b29e53e6af16b074fb55c56596
Aditya Yadav <147463086+yaditya3895@users.noreply.github.com>
2024-11-16 00:17:29 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,23 @@
+import Header from "./components/Header";
+import Fav from "./components/Fav";
+import Merchandise from "./components/Merchandise";
+import Explore from "./components/Explore";
+import Slide from "./components/Slide";
+import List from "./components/List";
+import Creators from "./components/Creators";
+
+
+
+ export default function Home() {
+  return (
+    <>
+    <Header/>
+    <Fav/>
+    <Merchandise/>
+    <Explore/>
+    <Slide/>
+    <List/>
+    <Creators/>
+    </>
+  );
+};
